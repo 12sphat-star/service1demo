51d198fd04540a692d039d1997b8f5dd22c0bc9e
Keith Robertson <keithrobertson@Keiths-MacBook-Pro.local>
2026-01-20 23:26:43 -0500

    LKG: Routing fixed, example-site added and compiling

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import ExampleSite from "./components/ExampleSite";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/example-site" element={<ExampleSite />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
